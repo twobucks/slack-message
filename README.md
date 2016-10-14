62b0ac560252404d52d2e12813b01bd4ec64f7ae
Dino Trojak <dinotrojak@gmail.com>
2016-10-14 20:51:47 +0200

    disabled additional properties

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Usage
 ```
-  slack-message "team-name" "channel" "My message here"
+  slack-message "token" "channel" "My message here"
 ```
 
 The idea for this package can be found [here.](https://github.com/mikaelbr/open-source-ideas/issues/2)
--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const Spinner = require('cli-spinner').Spinner;
 const args = process.argv.slice(2);
 
 function slackMessage(){
-  if(args.length !== 3) {
+  if(args.length < 3) {
     return log.error("All fields are required 'slack-message <token> <channel> <message>'");
+  } else if (args.length > 3) {
+    return log.error("No additional fields allowed 'slack-message <token> <channel> <message>'");
   }
   const message = {
     token: args[0],
